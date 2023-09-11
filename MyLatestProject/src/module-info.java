5ee44357b34beb820b3788e6839e9d6c82c72ac2
sryadav1619 <yadavsantosh1619@gmail.com>
2023-09-11 16:21:21 -0700

    MyNewFirstCommit
    This is only for practice purpose

---
+++ MyLatestProject/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Santosh yadav
+ *
+ */
+module MyLatestProject {
+}
---
+++ MyLatestProject/src/myPackage/GoodEnvironment.java
@@ -0,0 +1,9 @@
+package myPackage;
+
+public class GoodEnvironment {
+
+	public static void main(String[] args) {
+		
+		System.out.println("=== Good Environment is very important to makes eveyday happy ===");
+	}
+}
